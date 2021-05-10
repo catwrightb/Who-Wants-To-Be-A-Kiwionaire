50e09c1832532c4348dcce2026a68901a03430ad
Jada <jadatu13@icloud.com>
2021-05-10 19:15:17 +1200

    Added Database
    Added Database & DBManager

---
+++ src/Database/DBManager.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Jacob Tupe | 18018323
+ */
+public class DBManager {
+
+    private static final String USER_NAME = "questions"; //your DB username
+    private static final String PASSWORD = "questions"; //your DB password
+    private static final String URL = "jdbc:derby://localhost:1527/QuestionsDB";  //url of the DB host
+
+    Connection conn;
+
+    public DBManager() {
+        establishConnection();
+    }
+
+    public static void main(String[] args) {
+        DBManager dbManager = new DBManager();
+        System.out.println(dbManager.getConnection());
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    //Establish connection
+    public void establishConnection() {
+        //Establish a connection to Database
+        try {
+            conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            System.out.println(URL + " connected");
+        } catch (SQLException ex) {
+            System.err.println("SQLException: " + ex.getMessage());
+        }
+    }
+
+    public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public ResultSet queryDB(String sql) {
+
+        Connection connection = this.conn;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return resultSet;
+    }
+
+    public void updateDB(String sql) {
+
+        Connection connection = this.conn;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
