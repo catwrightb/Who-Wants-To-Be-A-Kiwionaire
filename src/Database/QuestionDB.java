02e1d4dfee07f68c1fd632213e7984ee121a9548
catwrightb <78883365+catwrightb@users.noreply.github.com>
2021-06-03 21:33:40 +1200

    updates to question date base

--- src/Database/QuestionDB.java
+++ src/Database/QuestionDB.java
@@ -27,10 +27,10 @@ public class QuestionDB {
         conn = dbManager.getConnection();
     }
 
-//    public static void main(String[] args) {
-//        QuestionDB users = new QuestionDB();
-//        users.printTableContents();
-//    }
+    public static void main(String[] args) {
+        QuestionDB users = new QuestionDB();
+        users.printTableContents();
+    }
 
     public ArrayList<Question> questionListCreator(){
         ResultSet rs = dbManager.queryDB("SELECT * FROM " + QUESTIONS_TABLE_NAME);
